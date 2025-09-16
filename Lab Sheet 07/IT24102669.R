bc269ca83cd8cc9265f5ceb180ba384f0b8e3865
IT24102669 <it24102669@my.sliit.lk>
2025-09-16 22:29:18 +0530

    IT24102669

---
+++ Lab Sheet 07/IT24102669.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\HP\\Desktop\\IT24102669")
+#Q1
+punif(15,min = 0, max = 40, lower.tail = TRUE)
+#Q2
+pexp(2,rate = 0.33, lower.tail = TRUE)
+#Q3
+#part1
+1-pnorm(130,mean = 100, sd=15, lower.tail = TRUE)
+#part2
+qnorm(0.95, mean = 100, sd = 15)
