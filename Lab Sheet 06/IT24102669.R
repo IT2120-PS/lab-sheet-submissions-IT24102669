d9711c1aacf885e52c9f5d5129390e8f423b5065
IT24102669 <it24102669@my.sliit.lk>
2025-09-19 16:29:39 +0530

    IT24102669

---
+++ Lab Sheet 06/IT24102669.R
@@ -0,0 +1,24 @@
+getwd()
+
+setwd("C:\\Users\\HP\\Desktop\\IT24102669")
+
+##Exercises
+#1
+
+#i)
+#Binomial Distribution
+#X ~ Bin(n = 50, p = 0.85)
+
+#ii)
+pbinom(46, size = 50, prob = 0.85, lower.tail = FALSE)
+
+#2
+
+#i)
+#X ~ Poisson(λ=12 per hour)
+
+#ii)
+#Poisson Distribution
+
+#iii)
+dpois(15, lambda = 12)
